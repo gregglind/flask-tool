3e6046904dea53b1e0b276081016fda811ff1339
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2011-02-24 19:28:14 -0500

    Greatly simplify and notes for future. `flasktool create app MyApp`

--- flasktool/console.py
+++ flasktool/console.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 from .extensions import FlaskExtension
 from .apps import FlaskApplication
 from flask import Flask
-from flaskext.script import prompt, prompt_choices, Manager
+from flaskext.script import prompt, prompt_choices, Manager, Option
 import string
 
 
@@ -13,65 +13,73 @@ def create_app(*args, **kwargs):
         return app
 
 manager = Manager(create_app, False)
-    
-@manager.option('--name', dest='name', required=False)
-@manager.option('--url', dest='url', required=False)
-@manager.option('--authorname', dest='author_name', required=False)
-@manager.option('--authoremail', dest='author_email', required=False)
-@manager.option('--license', dest='license', required=False,
-    help="BSD or MIT", default="BSD")
-@manager.option('--requires', dest='requires', required=False,
-    help="Comma separated list of required packages")
-@manager.option('--description', dest='author_email', required=False)
-def create_ext(name=None, url=None, author_name=None, author_email=None, 
-    license='BSD', requires=None, short_description=None):
-    """
-    Create the skeleton of a new Flask Extension.
-    
-    `flasktool create_ext`
-    `flasktool create_ext --name=MyExtension`
-    `flasktool create_ext --name=MyExtension --url=http://imlucas.com/flask-myextension`
-    `flasktool create_ext --name=MyExtension --authorname="Lucas"`
-    """
-    if not name:
-        name = prompt('Extension Name: (ie CouchDBKit)')
-    
-    if not url:
-        url = prompt('URL')
-    
-    if not author_name:
-        author_name = prompt('Author Name')
-    
-    if not author_email:
-        author_email = prompt('Author Email')
-    
-    license = prompt_choices('License', ('BSD','MIT','WTFPL'), 
-        default='BSD', resolve=string.upper)
-        
-    if not short_description:
-        short_description = prompt('Short Description')
-    
-    if not requires:
-        requires = prompt('Dependencies (comma separated, exclude Flask)')
-        
-    extension = FlaskExtension(name, url, author_name, author_email, 
-        short_description, requires, license)
 
-    print "###################"
-    print "Makin' the donuts..."
-    print "###################"
-    extension.boostrap()
-    print "###################"
-    print "Flask-%s created in %s" % (extension.name, extension.dir)
-    print "###################"
+@manager.option('-l', '--layout', default='factory')
+@manager.option(nargs=1, dest='name')
+@manager.option(nargs=1, dest='what')
+def create(what='app', name='MyAwesomeApp', layout='factory'):
+    """(ext|app) <name>"""
+    """
+    flasktool create ext 'Flask-MongoEngine'
 
-@manager.option('--name', dest='name', required=False)
-def create_app(name=None):
-    if not name:
-        name = prompt('App Name')
-    _app = FlaskApplication(name)
-    _app.bootstrap()
-    
+    flasktool create app MyAwesomeApp
+    flasktool create app MyAwesomeApp --layout=factory
+        - Create new folder.
+        - Make app.yaml and  add to new folder.
+        - Look at layout for template to use.
+        - cd to new app directory
+        - Create tasks, settings, etc.
+
+    flasktool create view account
+    flasktool create view api.account
+        - Check we're in an app and load app yaml.
+        - Default view imports in app.yaml
+        - Create tests.views.test_account.py
+
+    flasktool create view api.account
+        - Check we're in an app and load app yaml.
+
+    flasktool create model Account
+        - Check we're in an app and load app yaml.
+        - Create models.account.py.
+        - Create tests.models.account.py
+    """
+    what = what[0]
+    name = name[0]
+    if what == 'app':
+        _app = FlaskApplication(name)
+        _app.bootstrap()
+
+    elif what == 'ext':
+        if not url:
+            url = prompt('URL')
+        
+        if not author_name:
+            author_name = prompt('Author Name')
+        
+        if not author_email:
+            author_email = prompt('Author Email')
+        
+        license = prompt_choices('License', ('BSD','MIT','WTFPL'), 
+            default='BSD', resolve=string.upper)
+            
+        if not short_description:
+            short_description = prompt('Short Description')
+        
+        if not requires:
+            requires = prompt('Dependencies (comma separated, exclude Flask)')
+            
+        extension = FlaskExtension(name, url, author_name, author_email, 
+            short_description, requires, license)
+
+        print "###################"
+        print "Makin' the donuts..."
+        print "###################"
+        extension.boostrap()
+        print "###################"
+        print "Flask-%s created in %s" % (extension.name, extension.dir)
+        print "###################"
+        
 
 def run():
     manager.run()
