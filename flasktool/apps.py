13ee40b1ac01efa8c3909c0bf4679ae8c629b569
Gregg Lind <gregg.lind@gmail.com>
2011-08-16 12:26:31 -0500

    template='heavy' works, for a 'heavier' flask app, based on https://gist.github.com/1146774

--- flasktool/apps.py
+++ flasktool/apps.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import with_statement
 
 from os.path import exists, isfile, join
-from os import chmod
+from os import chmod, remove, removedirs
 from contextlib import closing
 from subprocess import Popen
 from shutil import copytree
@@ -25,6 +25,9 @@ class FlaskApplication(object):
         if self.layout == 'factory':
             return self._bootstrap_factory_app()
         
+        elif self.layout == 'heavy':
+            return self._bootstrap_heavy_factory_app()
+ 
         return self._bootstrap_simple_app()
     
     def _bootstrap_simple_app(self):
@@ -100,7 +103,29 @@ class FlaskApplication(object):
                     _create_file('__init__.py', 'apps/views/__init__.py.tpl', app=self)
                     for module_name in self.module_names:
                         _create_file('%s.py' % module_name, 'apps/views/module.py.tpl', app=self, module_name=module_name)
+
+    def _bootstrap_heavy_factory_app(self):
+        # it's a normal factory app, with extra!
+        self._bootstrap_factory_app()
+
+        self.callable = '%s:uwsgi'
         
-        
-        
-        
+        with _cd(self.dir):
+            with _cd(self.package_name):
+                _mkdir('utils')
+                _mkdir('apis')
+                _mkdir('adhoc')
+                _mkdir('tests')
+                _mkdir('docs')
+
+                with _cd('tests'):
+                    # Create tests
+                    _create_file('tests.py', 'apps/tests.py.tpl', app=self)
+
+        with _cd(self.dir):
+            # Create requirements file
+            _create_file('requirements.txt', 'apps/requirements.txt.heavy.tpl')
+
+            # remove test file, directory is better
+            remove('tests.py')
+
